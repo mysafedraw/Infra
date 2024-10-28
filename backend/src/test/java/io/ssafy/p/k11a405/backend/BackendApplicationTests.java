caf70b44889f3811aecf923123431249f2365798
junbeom-Son <junbeom247@gmail.com>
2024-10-28 15:01:09 +0900

    feat: create spring boot project

---
+++ backend/src/test/java/io/ssafy/p/k11a405/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ssafy.p.k11a405.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
