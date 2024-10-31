5c2b204db385101ccbfdd460e7c5c82433518122
junbeom-Son <junbeom247@gmail.com>
2024-10-31 15:44:16 +0900

    refactor: 불필요한 try catch문 제거

--- backend/src/main/java/io/ssafy/p/k11a405/backend/pubsub/MessagePublisher.java
+++ backend/src/main/java/io/ssafy/p/k11a405/backend/pubsub/MessagePublisher.java
@@ -1,6 +1,5 @@
 package io.ssafy.p.k11a405.backend.pubsub;
 
-import io.ssafy.p.k11a405.backend.dto.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -12,10 +11,6 @@ public class MessagePublisher {
     private final StringRedisTemplate redisTemplate;
 
     public void publish(String channel, Object message) {
-        try {
-            redisTemplate.convertAndSend(channel, message);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        redisTemplate.convertAndSend(channel, message);
     }
 }
