46c252393da95705671c744a5591d748b3c09d9c
junbeom-Son <junbeom247@gmail.com>
2024-10-31 15:37:43 +0900

    feat: ChatService 생성

---
+++ backend/src/main/java/io/ssafy/p/k11a405/backend/ChatService.java
@@ -0,0 +1,10 @@
+package io.ssafy.p.k11a405.backend;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ChatService {
+
+}
