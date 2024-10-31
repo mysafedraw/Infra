1bf418f4577f7b19ce2debf7aa9789f2ac9ff965
junbeom-Son <junbeom247@gmail.com>
2024-10-31 15:57:03 +0900

    feat: 메시지 전송

--- backend/src/main/java/io/ssafy/p/k11a405/backend/service/ChatService.java
+++ backend/src/main/java/io/ssafy/p/k11a405/backend/service/ChatService.java
@@ -1,5 +1,9 @@
 package io.ssafy.p.k11a405.backend.service;
 
+import io.ssafy.p.k11a405.backend.dto.SendChatRequestDTO;
+import io.ssafy.p.k11a405.backend.dto.SendChatResponseDTO;
+import io.ssafy.p.k11a405.backend.pubsub.MessagePublisher;
+import io.ssafy.p.k11a405.backend.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +11,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ChatService {
 
+    private final MessagePublisher messagePublisher;
+
+    private final DateUtil dateUtil;
+
+    public void sendMessage(String requestDestination, SendChatRequestDTO sendChatRequestDTO) {
+        String sentAt = dateUtil.getChatFormatTime();
+        String channel = requestDestination + "/" + sendChatRequestDTO.roomId();
+        SendChatResponseDTO sendChatResponseDTO = SendChatResponseDTO.builder()
+                .sendChatRequestDTO(sendChatRequestDTO).sentAt(sentAt).build();
+        messagePublisher.publish(channel, sendChatResponseDTO);
+    }
 }
