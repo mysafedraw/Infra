5d4b9816d098f3aa63d68e9cc49a36c5ad50fc1b
junbeom-Son <junbeom247@gmail.com>
2024-10-31 14:05:47 +0900

    refactor: 생성자 대신 requiredArgsConstructor 애노테이션 사용

--- backend/src/main/java/io/ssafy/p/k11a405/backend/controller/ChatController.java
+++ backend/src/main/java/io/ssafy/p/k11a405/backend/controller/ChatController.java
@@ -2,17 +2,15 @@ package io.ssafy.p.k11a405.backend.controller;
 
 import io.ssafy.p.k11a405.backend.dto.ChatMessage;
 import io.ssafy.p.k11a405.backend.pubsub.MessagePublisher;
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class ChatController {
 
     private final MessagePublisher messagePublisher;
-
-    public ChatController(MessagePublisher messagePublisher) {
-        this.messagePublisher = messagePublisher;
-    }
 
     @MessageMapping("/send") // 클라이언트가 WebSocket으로 보내는 메시지 경로
     public void sendMessage(ChatMessage message) {
