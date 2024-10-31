e7ebd835a5e2ef2cf4419f2a7e26f53c55202270
junbeom-Son <junbeom247@gmail.com>
2024-10-31 15:41:22 +0900

    refactor: 파라미터 타입 변경

--- backend/src/main/java/io/ssafy/p/k11a405/backend/pubsub/MessagePublisher.java
+++ backend/src/main/java/io/ssafy/p/k11a405/backend/pubsub/MessagePublisher.java
@@ -11,7 +11,7 @@ public class MessagePublisher {
 
     private final StringRedisTemplate redisTemplate;
 
-    public void publish(String channel, ChatMessage message) {
+    public void publish(String channel, Object message) {
         try {
             System.out.println("Publishing to Redis channel " + channel + ": " + message); // 디버깅 로그
             redisTemplate.convertAndSend(channel, message);
