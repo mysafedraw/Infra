8c536fd2d24a14054ddc3d9a20ac65f79a76ff80
junbeom-Son <junbeom247@gmail.com>
2024-11-01 17:36:00 +0900

    refactor: 필드 변경

--- backend/src/main/java/io/ssafy/p/k11a405/backend/dto/ExplainResponseDTO.java
+++ backend/src/main/java/io/ssafy/p/k11a405/backend/dto/ExplainResponseDTO.java
@@ -1,6 +1,7 @@
 package io.ssafy.p.k11a405.backend.dto;
 
+import java.util.List;
+
 public record ExplainResponseDTO(
-        String roomId,
-        String userId
+        List<String> userIds
 ) {}
