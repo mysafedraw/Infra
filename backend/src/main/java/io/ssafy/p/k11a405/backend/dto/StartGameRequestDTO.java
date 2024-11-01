901244fa1a0e9e76509011f141122aca4200e73c
junbeom-Son <junbeom247@gmail.com>
2024-11-01 16:46:32 +0900

    feat: timeLimit 필드 추가

--- backend/src/main/java/io/ssafy/p/k11a405/backend/dto/StartGameRequestDTO.java
+++ backend/src/main/java/io/ssafy/p/k11a405/backend/dto/StartGameRequestDTO.java
@@ -3,5 +3,6 @@ package io.ssafy.p.k11a405.backend.dto;
 public record StartGameRequestDTO(
         String roomId,
         Integer scenarioId,
-        Integer stageNumber
+        Integer stageNumber,
+        Integer timeLimit
 ) {}
