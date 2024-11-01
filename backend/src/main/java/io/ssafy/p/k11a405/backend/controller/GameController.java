e08e78eea1ed6f7e342fc07e00139f9c278cf5ad
junbeom-Son <junbeom247@gmail.com>
2024-11-01 16:47:01 +0900

    feat: 게임 시작

--- backend/src/main/java/io/ssafy/p/k11a405/backend/controller/GameController.java
+++ backend/src/main/java/io/ssafy/p/k11a405/backend/controller/GameController.java
@@ -1,11 +1,21 @@
 package io.ssafy.p.k11a405.backend.controller;
 
+import io.ssafy.p.k11a405.backend.dto.StartGameRequestDTO;
+import io.ssafy.p.k11a405.backend.service.GameService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class GameController {
+
+    private final GameService gameService;
+
+    @MessageMapping("/start")
+    public void startGame(StartGameRequestDTO startGameRequestDTO) {
+        gameService.startGame(startGameRequestDTO);
+    }
 }
--- backend/src/main/java/io/ssafy/p/k11a405/backend/service/GameService.java
+++ backend/src/main/java/io/ssafy/p/k11a405/backend/service/GameService.java
@@ -1,5 +1,8 @@
 package io.ssafy.p.k11a405.backend.service;
 
+import io.ssafy.p.k11a405.backend.dto.StartGameRequestDTO;
+import io.ssafy.p.k11a405.backend.dto.StartGameResponseDTO;
+import io.ssafy.p.k11a405.backend.pubsub.MessagePublisher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,4 +11,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Slf4j
 public class GameService {
+
+    private final String redisKeyPrefix = "games:";
+
+    private final MessagePublisher messagePublisher;
+
+    public void startGame(StartGameRequestDTO startGameRequestDTO) {
+        String channelName = redisKeyPrefix + startGameRequestDTO.roomId();
+        // db에서 scenarioDialog 가져오기
+
+        String tmpScenarioDialog = "헉 불이났어요!!";
+        StartGameResponseDTO startGameResponseDTO = new StartGameResponseDTO(tmpScenarioDialog);
+        messagePublisher.publish(channelName, startGameResponseDTO);
+    }
 }
