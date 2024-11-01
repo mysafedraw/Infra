1804569759f84dae7d7d717e5165b61a3db967e3
junbeom-Son <junbeom247@gmail.com>
2024-11-01 17:40:08 +0900

    feat: action 필드 추가

--- backend/src/main/java/io/ssafy/p/k11a405/backend/dto/ExplainResponseDTO.java
+++ backend/src/main/java/io/ssafy/p/k11a405/backend/dto/ExplainResponseDTO.java
@@ -3,5 +3,6 @@ package io.ssafy.p.k11a405.backend.dto;
 import java.util.List;
 
 public record ExplainResponseDTO(
-        List<String> userIds
+        List<String> userIds,
+        GameAction action
 ) {}
