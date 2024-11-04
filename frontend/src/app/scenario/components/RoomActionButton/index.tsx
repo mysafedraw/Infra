e04f75a1914d71507fdfa9c53a81c59dcbf07781
yukyung <kyg0954@naver.com>
2024-11-04 16:06:48 +0900

    comment: 필요없는 주석 제거

--- frontend/src/app/scenario/components/RoomActionButton/index.tsx
+++ frontend/src/app/scenario/components/RoomActionButton/index.tsx
@@ -9,7 +9,6 @@ export default function RoomActionButton({
     <button
       className={`${bgColor} group flex justify-center items-center flex-col w-[26rem] h-96 rounded-2xl border-white border-[5px] gap-10 shadow-button-inactive relative hover:scale-105 hover:shadow-button-active transition-transform duration-200 ease-in-out`}
     >
-      {/* <span className="group-hover:opacity-80 transition-all duration-200 ease-in-out w-80 h-3 bg-white opacity-0 absolute top-3 rounded-full" /> */}
       <div className="flex flex-col w-60 bg-white h-44 rounded-lg group-hover:shadow-md">
         <span>캐릭터</span>
       </div>
