4b10c7da39541f8572c2aeeb79af2d6b51153559
yunva17 <yunva17@naver.com>
2024-11-04 09:20:18 +0900

    style: 코드 스타일 정리

--- frontend/src/app/enter/components/EnterInput/index.tsx
+++ frontend/src/app/enter/components/EnterInput/index.tsx
@@ -16,7 +16,7 @@ export default function EnterInput() {
         onChange={handleRoomCodeChange}
         type="text"
         placeholder="방코드 입력"
-        className="w-full  bg-transparent text-2xl text-center text-primary-800 outline-none placeholder:text-2xl placeholder:text-primary-700"
+        className="w-full bg-transparent text-2xl text-center text-primary-800 outline-none placeholder:text-2xl placeholder:text-primary-700"
       />
       <hr className="w-3/4 border-b-2 border-white mt-2" />
     </div>
