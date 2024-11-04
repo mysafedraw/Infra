f01ab307242a568871876880c2b42fa5074d81a0
yunva17 <yunva17@naver.com>
2024-11-04 09:14:25 +0900

    feat: 나무 버튼 컴포넌트 추가

---
+++ frontend/src/app/_components/SignButton/index.tsx
@@ -0,0 +1,25 @@
+import { MouseEventHandler } from 'react'
+import Image from 'next/image'
+
+export default function SignButton({
+  onClick,
+  content,
+}: {
+  onClick: MouseEventHandler<HTMLButtonElement>
+  content: string
+}) {
+  return (
+    <button onClick={onClick} className="relative w-[190px] h-[100px]">
+      <Image
+        src="/images/wooden_arrow_sign.png"
+        alt="나무 버튼"
+        width={200}
+        height={100}
+        className="rounded-lg object-cover"
+      />
+      <span className="absolute top-[38px] left-[90px] transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl font-bold">
+        {content}
+      </span>
+    </button>
+  )
+}
--- frontend/src/app/enter/components/EnterInput/index.tsx
+++ frontend/src/app/enter/components/EnterInput/index.tsx
@@ -3,13 +3,13 @@
 import { useState } from 'react'
 
 export default function EnterInput() {
-  const [roomCode, setRoomCode] = useState<number>()
+  const [roomCode, setRoomCode] = useState<string>('')
 
   const handleRoomCodeChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setRoomCode(Number(e.target.value))
+    setRoomCode(e.target.value)
 
   return (
-    <div className="flex bg-primary-600 border-4 border-primary-700 w-2/5 h-28 rounded-xl flex-col justify-center items-center">
+    <div className="flex bg-primary-600 border-4 border-primary-700 w-2/5 h-36 rounded-xl flex-col justify-center items-center">
       {/* 입력 필드 */}
       <input
         value={roomCode}
