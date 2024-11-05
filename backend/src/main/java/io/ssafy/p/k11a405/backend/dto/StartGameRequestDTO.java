db6a01c03eb051733c2e492a82c6c62701b57fb7
junbeom-Son <junbeom247@gmail.com>
2024-11-05 16:56:37 +0900

    refactor: 게임 시작 요청시 시나리오 아이디 삭제

--- backend/src/main/java/io/ssafy/p/k11a405/backend/dto/StartGameRequestDTO.java
+++ backend/src/main/java/io/ssafy/p/k11a405/backend/dto/StartGameRequestDTO.java
@@ -2,7 +2,6 @@ package io.ssafy.p.k11a405.backend.dto;
 
 public record StartGameRequestDTO(
         String roomId,
-        String scenarioId,
         Integer stageNumber,
         Integer timeLimit
 ) {}
