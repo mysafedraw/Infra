e6d27ccc7763574ad33e3a779a9f70c3da82f65a
junbeom-Son <junbeom247@gmail.com>
2024-11-06 13:27:17 +0900

    rename: 이미지 redis에 저장할 필드명 변경

--- backend/src/main/java/io/ssafy/p/k11a405/backend/service/ImageService.java
+++ backend/src/main/java/io/ssafy/p/k11a405/backend/service/ImageService.java
@@ -13,6 +13,8 @@ import java.io.IOException;
 @Slf4j
 public class ImageService {
 
+    private final String drawSrcField = "drawSrc";
+
     private final StringRedisTemplate stringRedisTemplate;
 
     public void uploadAnswerDrawing(MultipartFile file, String userId) throws IOException {
@@ -20,6 +22,6 @@ public class ImageService {
         // 실제로는 이미지를 저장하고, 그 경로를 가져와야 한다.
         String drawingSrc = "https://cdn.sortiraparis.com/images/1001/69688/1076636-venom-the-last-dance-le-dernier-chapitre-de-la-trilogie-marvel-avec-tom-hardy-se-devoile.jpg";
 
-        stringRedisTemplate.opsForHash().put(userKey, "drawingSrc", drawingSrc);
+        stringRedisTemplate.opsForHash().put(userKey, drawSrcField, drawingSrc);
     }
 }
