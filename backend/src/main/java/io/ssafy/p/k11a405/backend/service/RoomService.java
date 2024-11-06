9bc4581b31ace25f93d7cdc20f8b5db1cc99aa78
junbeom-Son <junbeom247@gmail.com>
2024-11-06 14:17:45 +0900

    feat: 모든 정답 확인 채널 추가

--- backend/src/main/java/io/ssafy/p/k11a405/backend/service/RoomService.java
+++ backend/src/main/java/io/ssafy/p/k11a405/backend/service/RoomService.java
@@ -4,6 +4,7 @@ import io.ssafy.p.k11a405.backend.common.RedisSubscriber;
 import io.ssafy.p.k11a405.backend.dto.RoomEventMessage;
 import io.ssafy.p.k11a405.backend.dto.RoomResponseDTO;
 import io.ssafy.p.k11a405.backend.dto.SendChatResponseDTO;
+import io.ssafy.p.k11a405.backend.dto.game.CheckAllAnswersResponseDTO;
 import io.ssafy.p.k11a405.backend.dto.game.ExplainResponseDTO;
 import io.ssafy.p.k11a405.backend.dto.game.StartGameResponseDTO;
 import io.ssafy.p.k11a405.backend.pubsub.GenericMessagePublisher;
@@ -51,10 +52,12 @@ public class RoomService {
         String chatChannel = "chat:" + roomId;
         String gameStartChannel = "games:start:" + roomId;
         String gameExplainQueue = "games:explainQueue:" + roomId;
+        String gameAllAnswersChannel = "games:allAnswers:" + roomId;
         redisSubscriber.subscribeToChannel(channelName, RoomEventMessage.class, "/rooms/" + roomId);
         redisSubscriber.subscribeToChannel(chatChannel, SendChatResponseDTO.class, "/chat/" + roomId);
         redisSubscriber.subscribeToChannel(gameStartChannel, StartGameResponseDTO.class, "/games/" + roomId);
         redisSubscriber.subscribeToChannel(gameExplainQueue, ExplainResponseDTO.class, "/games/" + roomId);
+        redisSubscriber.subscribeToChannel(gameAllAnswersChannel, CheckAllAnswersResponseDTO.class, "/games/" + roomId);
 
         // 방 정보에 방장 세션 ID 포함
 //        String ownerSessionId = stringRedisTemplate.opsForHash().get("session:user", ownerId).toString();
