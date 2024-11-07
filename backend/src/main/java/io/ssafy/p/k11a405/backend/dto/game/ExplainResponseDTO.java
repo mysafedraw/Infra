98ae7b94cbb1a3d9e7bdd7f92a54e99c5e052507
junbeom-Son <junbeom247@gmail.com>
2024-11-07 15:33:22 +0900

    fix: 필드명 변경

--- backend/src/main/java/io/ssafy/p/k11a405/backend/dto/game/ExplainResponseDTO.java
+++ backend/src/main/java/io/ssafy/p/k11a405/backend/dto/game/ExplainResponseDTO.java
@@ -3,6 +3,6 @@ package io.ssafy.p.k11a405.backend.dto.game;
 import java.util.List;
 
 public record ExplainResponseDTO(
-        List<AnswerStatusResponseDTO> userIds,
+        List<AnswerStatusResponseDTO> waitingQueue,
         GameAction action
 ) {}
