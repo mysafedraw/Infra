b95b648b815e82ac55e72b9dfd761cac5b974066
dana019 <dana019@naver.com>
2024-11-07 14:33:36 +0900

    feat: 전체 답안 확인 웹소켓 API 연동

--- frontend/src/app/scenario/result/host/page.tsx
+++ frontend/src/app/scenario/result/host/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import Image from 'next/image'
-import ScoredBoard from '@/app/scenario/result/components/ScoredBoard'
 import ExplainQueueBoard from '@/app/scenario/result/host/components/ExplainQueueBoard'
 import VotingSidebar from '@/app/scenario/result/components/VotingSidebar'
 import VotingResult from '@/app/scenario/result/host/components/VotingResult'
 import { useState } from 'react'
+import AllAnswers from '@/app/scenario/result/components/AllAnswers'
 
 interface AnswerData {
   id: number
@@ -14,59 +14,6 @@ interface AnswerData {
   characterImage: string
   drawingImage: string
 }
-
-// 임시 데이터 리스트
-const answerData: AnswerData[] = [
-  {
-    id: 1,
-    isCorrect: false,
-    nickname: '이구역그림짱은나야',
-    characterImage: '/images/tiger.png',
-    drawingImage: '/images/drawing.png',
-  },
-  {
-    id: 2,
-    isCorrect: true,
-    nickname: '햄벅유경',
-    characterImage: '/images/rabbit.png',
-    drawingImage: '/images/drawing.png',
-  },
-  {
-    id: 3,
-    isCorrect: true,
-    nickname: '김지허니',
-    characterImage: '/images/rabbit.png',
-    drawingImage: '/images/drawing.png',
-  },
-  {
-    id: 4,
-    isCorrect: true,
-    nickname: '핑구핑',
-    characterImage: '/images/tiger.png',
-    drawingImage: '/images/drawing.png',
-  },
-  {
-    id: 5,
-    isCorrect: true,
-    nickname: '동원참치',
-    characterImage: '/images/tiger.png',
-    drawingImage: '/images/drawing.png',
-  },
-  {
-    id: 6,
-    isCorrect: false,
-    nickname: '아가주호',
-    characterImage: '/images/rabbit.png',
-    drawingImage: '/images/drawing.png',
-  },
-  {
-    id: 7,
-    isCorrect: false,
-    nickname: 'hand given tiger',
-    characterImage: '/images/tiger.png',
-    drawingImage: '/images/drawing.png',
-  },
-]
 
 const waitingData: AnswerData[] = [
   {
@@ -134,14 +81,10 @@ export default function ScenarioResultHost() {
         ))}
       </button>
 
-      <div className="-ml-6 mr-auto mt-6 bg-wood bg-cover w-72 py-3 pr-5 text-right text-4xl text-white rounded-r-lg shadow-lg">
+      <div className="-ml-6 mr-auto mt-6 bg-wood bg-cover w-72 py-3 pr-5 text-right text-4xl text-white rounded-r-lg shadow-lg mb-4">
         전체 답변
       </div>
-      <div className="grid grid-cols-3 gap-x-6 mt-4">
-        {answerData.map((data) => (
-          <ScoredBoard key={data.id} data={data} />
-        ))}
-      </div>
+      <AllAnswers />
 
       <VotingSidebar role="host" />
       {isVotingResultVisible && (
