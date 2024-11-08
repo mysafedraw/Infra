41ab29ac678f950043009823761cef24de56230a
dana019 <dana019@naver.com>
2024-11-08 17:51:26 +0900

    fix: 전체 답안 조회 파라미터 수정

--- frontend/src/app/scenario/result/components/AllAnswers/index.tsx
+++ frontend/src/app/scenario/result/components/AllAnswers/index.tsx
@@ -54,7 +54,7 @@ export default function AllAnswers() {
       sendMessage(
         '/games/answers',
         JSON.stringify({
-          roomNumber,
+          roomId: roomNumber,
         }),
       )
     }
