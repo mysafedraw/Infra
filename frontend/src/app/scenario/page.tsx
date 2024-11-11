bb2a1414b588a12fe589be59f4def1f7052d54f0
yukyung <kyg0954@naver.com>
2024-11-11 09:16:09 +0900

    feat: asset 추가

--- frontend/src/app/scenario/page.tsx
+++ frontend/src/app/scenario/page.tsx
@@ -1,12 +1,38 @@
-import Link from 'next/link'
+import Map from '@/app/scenario/components/Map'
 
-export default function Scenario() {
+export interface Scenario {
+  id: number
+  name: string
+  description: string
+  imgUrl: string
+}
+
+async function fetchScenario(): Promise<Scenario[]> {
+  try {
+    const response = await fetch(`https://mysafedraw.site/api/scenarios`, {
+      method: 'GET',
+      cache: 'no-store',
+    })
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch scenarios')
+    }
+
+    const result = await response.json()
+
+    return result.data
+  } catch (error) {
+    console.error('Error fetching scenarios:', error)
+    return []
+  }
+}
+
+export default async function Home() {
+  const scenarios = await fetchScenario()
+
   return (
-    <div>
-      <p>시나리오 메인페이지</p>
-      <Link href="/scenario/1">
-        <button>시나리오 1번으로 이동</button>
-      </Link>
+    <div className="w-screen h-screen">
+      <Map scenarios={scenarios} />
     </div>
   )
 }
