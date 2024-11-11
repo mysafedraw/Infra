43a9bc5b6d148c552300503240cbdc42866312b4
yukyung <kyg0954@naver.com>
2024-11-11 09:10:54 +0900

    feat: user 생성하는 api 연결

--- frontend/src/app/(home)/@modal/(.)nickname/set/page.tsx
+++ frontend/src/app/(home)/@modal/(.)nickname/set/page.tsx
@@ -5,9 +5,63 @@ import SirenIcon from '/public/icons/siren.svg'
 import HelmetIcon from '/public/icons/helmet.svg'
 import ProhibitionIcon from '/public/icons/prohibition.svg'
 import BlinkerIcon from '/public/icons/blinker.svg'
+import { useUser } from '@/app/_contexts/UserContext'
+import { useEffect, useState } from 'react'
 
 export default function SetNicknameModal() {
   const router = useRouter()
+  const [nickname, setNickname] = useState('')
+  const { user, setUser } = useUser()
+
+  const handleChangeNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNickname(e.target.value)
+  }
+
+  const handleClickEnterButton = async () => {
+    if (!user || nickname.trim().length <= 0) {
+      alert('이름을 정해주세요')
+      return
+    }
+    setUser({ ...user, nickname })
+  }
+
+  const postUser = async (): Promise<any> => {
+    const response = await fetch(`https://mysafedraw.site/api/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+
+    if (!response.ok) {
+      throw new Error('Failed to post user')
+    }
+
+    const result = await response.json()
+    return result
+  }
+
+  useEffect(() => {
+    const postUserInfo = async () => {
+      try {
+        if (user?.nickname) {
+          const userInfo = await postUser()
+          setUser({
+            ...user,
+            avatarImg: userInfo.avatarImg,
+            userId: userInfo.userId,
+          })
+          router.back()
+        }
+      } catch (error) {
+        console.error('Failed to fetch User:', error)
+      }
+    }
+
+    postUserInfo()
+  }, [user?.nickname])
 
   return (
     <div className="fixed top-0 w-full h-screen bg-[rgba(0,0,0,0.6)] inline-flex flex-col items-center justify-center gap-11 pb-[15vh] z-30">
@@ -19,12 +73,14 @@ export default function SetNicknameModal() {
         <h1 className="text-5xl text-text">이름을 정해보자</h1>
         <input
           type="text"
+          value={nickname}
           placeholder="깜찍이"
           className="border-[5px] border-primary-500 py-4 text-center rounded-xl text-4xl placeholder:text-primary-200 outline-none w-full text-primary-700"
+          onChange={handleChangeNickname}
         />
         <button
           className="gap-8 bg-primary-600 border-[5px] text-4xl border-primary-700 rounded-md text-white flex w-full items-center py-2 justify-center"
-          onClick={() => router.back()}
+          onClick={handleClickEnterButton}
         >
           <SirenIcon className="h-16" />
           안전 나라로 접속하기
--- frontend/src/app/(home)/nickname/page.tsx
+++ frontend/src/app/(home)/nickname/page.tsx
@@ -5,9 +5,21 @@ import { Canvas } from '@react-three/fiber'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 import CharacterModel from '@/app/(home)/components/CharacterModel'
+import { useUser } from '@/app/_contexts/UserContext'
+
+const CHARACTER_ASSETS: Record<number, string> = {
+  1: '/assets/character/dog.glb',
+  2: '/assets/character/tiger.glb',
+  3: '/assets/character/bunny.glb',
+  4: '/assets/character/fox.glb',
+  5: '/assets/character/penguin.glb',
+  6: '/assets/character/unicorn.glb',
+  7: '/assets/character/cat.glb',
+}
 
 export default function UserNickname() {
   const router = useRouter()
+  const { user } = useUser()
 
   useEffect(() => {
     router.push('/nickname/set')
@@ -26,11 +38,16 @@ export default function UserNickname() {
           maxPolarAngle={Math.PI / 2.3}
           minPolarAngle={Math.PI / 2.3}
         />
-        <CharacterModel url="/assets/character/cat.glb" />
+        {user && user.avatarId ? (
+          <CharacterModel url={CHARACTER_ASSETS[user?.avatarId]} />
+        ) : null}
       </Canvas>
-      {/* 닉네임 작성 여부에 따라 visible 설정 필요 */}
-      <div className="flex flex-col gap-1 bg-primary-600 border-primary-700 border-[5px] text-white rounded-lg text-3xl w-full px-10 py-5 max-w-[60rem]">
-        <p>알겠어. 나는 호돌이야!</p>
+      <div
+        className={`${user && user.nickname ? 'visible' : 'invisible'} flex flex-col gap-1 bg-primary-600 border-primary-700 border-[5px] text-white rounded-lg text-3xl w-full px-10 py-5 max-w-[60rem]`}
+      >
+        <p>
+          알겠어. 나는 <strong>{user?.nickname}</strong>야!
+        </p>
         <p>지금부터 안전에 대해서 배우러 안전 나라로 떠나보자~</p>
       </div>
     </div>
