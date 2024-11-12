c7095065a9f716ed33d80ab3f750d24418b43d3a
dongwonkwon <ehddnjs0710@gmail.com>
2024-11-13 00:45:14 +0900

    hotfix: 헬스체크용 컨트롤러

---
+++ backend/src/main/java/io/ssafy/p/k11a405/backend/controller/HealthCheckController.java
@@ -0,0 +1,19 @@
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<Map<String, String>> healthCheck() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "UP");
+        return ResponseEntity.ok(response);
+    }
+}
