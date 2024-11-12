8f3e2cc043097a94c81095e675772c66bd16f071
yukyung <kyg0954@naver.com>
2024-11-12 17:47:52 +0900

    feat: 2번 시나리오 초기 페이지 UI

--- frontend/src/app/scenario/components/CharacterDialogue/index.tsx
+++ frontend/src/app/scenario/components/CharacterDialogue/index.tsx
@@ -1,3 +1,4 @@
+import { useUser } from '@/app/_contexts/UserContext'
 import Image from 'next/image'
 
 export default function CharacterDialogue({
@@ -5,6 +6,7 @@ export default function CharacterDialogue({
 }: {
   speechText: string
 }) {
+  const { user } = useUser()
   return (
     <div className="absolute bottom-2 left-6 right-6">
       <div className="flex items-end max-w-5xl mx-auto">
@@ -13,7 +15,7 @@ export default function CharacterDialogue({
           <div className="absolute right-[-11px] top-1/2 transform -translate-y-1/2 w-4 h-4 bg-white border-t-4 border-r-4 border-primary-600 rotate-45"></div>
         </div>
         <Image
-          src="/images/tiger.png"
+          src={user?.avatarImg}
           alt="character-dialogue"
           width={200}
           height={200}
