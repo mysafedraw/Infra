c263d3923ab4f56eeeda28ebe2be6e76453d6e5b
dongwonkwon <ehddnjs0710@gmail.com>
2024-11-13 01:43:13 +0900

    hotfix: 헬스체크

--- backend/src/main/java/io/ssafy/p/k11a405/backend/controller/HealthCheckController.java
+++ backend/src/main/java/io/ssafy/p/k11a405/backend/controller/HealthCheckController.java
@@ -1,3 +1,5 @@
+package io.ssafy.p.k11a405.backend.controller;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
