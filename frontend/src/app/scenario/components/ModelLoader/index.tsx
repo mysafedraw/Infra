9a18ed2d62a0119ffa1064d17e9d8fb53e211263
yukyung <kyg0954@naver.com>
2024-11-12 17:41:05 +0900

    fix: modelLoader 변경

--- frontend/src/app/scenario/components/ModelLoader/index.tsx
+++ frontend/src/app/scenario/components/ModelLoader/index.tsx
@@ -3,6 +3,7 @@
 import { useRef, useEffect } from 'react'
 import { useGLTF } from '@react-three/drei'
 import * as THREE from 'three'
+import { useFrame } from '@react-three/fiber'
 
 interface ModelLoaderProps {
   path: string // 모델 파일 경로
@@ -23,13 +24,21 @@ export default function ModelLoader({
   receiveShadow = true,
   onLoad,
 }: ModelLoaderProps) {
-  const gltf = useGLTF(path)
+  const { scene: originalScene, animations } = useGLTF(path)
+  const scene = originalScene.clone()
   const modelRef = useRef<THREE.Group>(null)
+  const mixerRef = useRef<THREE.AnimationMixer | null>(null)
 
   useEffect(() => {
-    if (gltf.scene) {
-      // 모델이 로드될 때 실행하는 추가 작업
-      gltf.scene.traverse((child) => {
+    if (scene && animations.length > 0) {
+      mixerRef.current = new THREE.AnimationMixer(scene)
+      const action = mixerRef.current.clipAction(animations[0])
+      action.setLoop(THREE.LoopRepeat, Infinity)
+      action.clampWhenFinished = true
+      action.timeScale = 1
+      action.play()
+
+      scene.traverse((child) => {
         if (child instanceof THREE.Mesh) {
           child.castShadow = castShadow
           child.receiveShadow = receiveShadow
@@ -39,13 +48,24 @@ export default function ModelLoader({
       if (onLoad && modelRef.current) {
         onLoad(modelRef.current)
       }
+
+      return () => {
+        action.stop()
+        mixerRef.current?.uncacheClip(animations[0])
+      }
     }
-  }, [gltf, castShadow, receiveShadow, onLoad])
+  }, [animations, scene, castShadow, receiveShadow, onLoad])
+
+  useFrame((_, delta) => {
+    if (mixerRef.current) {
+      mixerRef.current.update(delta)
+    }
+  })
 
   return (
     <primitive
       ref={modelRef}
-      object={gltf.scene.clone()}
+      object={scene}
       position={position}
       scale={scale}
       rotation={rotation}
