ec28dcc70d8e2150fb01bdca468e0a1f31b74325
dana019 <dana019@naver.com>
2024-11-13 09:16:58 +0900

    feat: 억울해요 버튼 클릭 이벤트에 음성 채팅방 참여 기능 추가

--- frontend/src/app/scenario/result/participant/components/AppealButton/index.tsx
+++ frontend/src/app/scenario/result/participant/components/AppealButton/index.tsx
@@ -3,10 +3,12 @@
 import { useWebSocketContext } from '@/app/_contexts/WebSocketContext'
 import { useEffect, useState } from 'react'
 import { useUser } from '@/app/_contexts/UserContext'
+import { useLiveKit } from '@/app/_contexts/LiveKitContext'
 
 export default function AppealButton() {
   const { user } = useUser()
   const { sendMessage, registerCallback } = useWebSocketContext()
+  const { voiceRoom, joinVoiceRoom } = useLiveKit()
   const [roomId, setRoomId] = useState<string | null>(null)
   const [isWaiting, setIsWaiting] = useState(false) // 발언 순서를 기다리는 상태
 
@@ -18,7 +20,19 @@ export default function AppealButton() {
     })
   }, [registerCallback, roomId])
 
-  const handleAppeal = () => {
+  const handleAppeal = async () => {
+    // 음성 채팅 참여
+    await joinVoiceRoom(roomId!, user?.userId || '')
+
+    // 마이크 음소거 설정 (발언권을 받기 전까지)
+    voiceRoom?.localParticipant.audioTrackPublications.forEach(
+      (publication) => {
+        if (publication.track) {
+          publication.track.mute()
+        }
+      },
+    )
+
     const message = JSON.stringify({ roomId, userId: user?.userId })
     sendMessage('/games/explanation-queue', message)
   }
