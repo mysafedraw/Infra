4ee4c40f5f25b53efdfef6d9142c93e90bb8c460
yukyung <kyg0954@naver.com>
2024-11-14 11:35:59 +0900

    feat: 화재 시나리오 5단계 병원을 그려서 성공했을 경우 상호작용

--- frontend/src/app/scenario/[id]/situation/step5/success/hospital/page.tsx
+++ frontend/src/app/scenario/[id]/situation/step5/success/hospital/page.tsx
@@ -4,9 +4,16 @@ import { Canvas } from '@react-three/fiber'
 import StoryLayout from '@/app/scenario/[id]/situation/components/StoryLayout'
 import ModelLoader from '@/app/scenario/[id]/situation/components/ModelLoader'
 import ARController from '@/app/scenario/[id]/situation/components/ARController'
+import { useState } from 'react'
+import { Html } from '@react-three/drei'
+
+type PenguinState = 'sick' | 'healthy'
 
 export default function HospitalScene() {
-  const speechText = '내가 병원에 빨리 데려다줘서 친구가 살 수 있을 것 같아!!'
+  const [speechText, setSpeechText] = useState(
+    '내가 병원에 빨리 데려다줘서 친구가 살 수 있을 것 같아!!',
+  )
+  const [penguinState, setPenguinState] = useState<PenguinState>('sick')
 
   return (
     <StoryLayout speechText={speechText} isSpeechVisible>
@@ -31,20 +38,41 @@ export default function HospitalScene() {
           intensity={4}
           color="#ffffff"
         />
-        <>
-          <ModelLoader
-            path="/assets/scenario/penguin-sick.glb"
-            position={[-1, -1, 0]}
-            scale={[0.025, 0.025, 0.025]}
-            rotation={[-0.5, 0.5, 0]}
-          />
-        </>
+
+        <ModelLoader
+          path={
+            penguinState === 'sick'
+              ? '/assets/scenario/penguin-sick.glb'
+              : '/assets/character/penguin.glb'
+          }
+          position={[-1, -1, 0]}
+          scale={[0.025, 0.025, 0.025]}
+          rotation={[-0.5, 0.5, 0]}
+        />
+
         <ModelLoader
           path="/assets/scenario/hospital.glb"
           position={[2, 1, 0]}
           scale={[0.0027, 0.0027, 0.0027]}
           rotation={[0.2, -2.1, -0.1]}
+          onClick={() => {
+            setPenguinState('healthy')
+            setSpeechText(
+              '와 병원에서 치료를 받아서 내 친구 펭펭이가 살아났어!',
+            )
+          }}
         />
+        {penguinState === 'sick' ? (
+          <Html position={[2, 1, 0]} style={{ pointerEvents: 'none' }}>
+            <div className="relative flex justify-center items-center">
+              <div className="rounded-full border-[10px] border-dashed border-primary-500 w-96 h-96 absolute"></div>
+              <div className="rounded-full border-[10px] border-dashed border-primary-500 w-80 h-80 absolute"></div>
+              <p className="bg-primary-500 whitespace-nowrap py-5 px-8 text-2xl rounded-lg shadow-md absolute top-0 left-0">
+                병원을 클릭해주세요
+              </p>
+            </div>
+          </Html>
+        ) : null}
 
         <ARController>
           <ambientLight intensity={0.5} />
