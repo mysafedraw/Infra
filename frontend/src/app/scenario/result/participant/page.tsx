05c64c5c9ce6553053c30949b37e3f4ab5832558
dana019 <dana019@naver.com>
2024-11-14 14:48:10 +0900

    feat: 내 그림 조회 웹소켓 API 연결

--- frontend/src/app/scenario/result/participant/page.tsx
+++ frontend/src/app/scenario/result/participant/page.tsx
@@ -6,11 +6,36 @@ import AllAnswers from '@/app/scenario/result/components/AllAnswers'
 import AppealButton from '@/app/scenario/result/participant/components/AppealButton'
 import { useWebSocketContext } from '@/app/_contexts/WebSocketContext'
 import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { useUser } from '@/app/_contexts/UserContext'
+import { AnswerData } from '@/app/scenario/result/types/answerTypes'
 
 export default function ScenarioResultParticipant() {
-  const { registerCallback } = useWebSocketContext()
   const router = useRouter()
+  const { sendMessage, registerCallback } = useWebSocketContext()
+  const { user } = useUser()
+  const [roomId, setRoomId] = useState<string | null>(null)
+  const [myDrawing, setMyDrawing] = useState<AnswerData>()
+  useEffect(() => {
+    setRoomId(localStorage.getItem('roomId'))
+  }, [])
+
+  useEffect(() => {
+    const message = JSON.stringify({ userId: user?.userId })
+    sendMessage('/games/my-drawing', message)
+
+    registerCallback(`/games/${user?.userId}`, 'MY_DRAWING', (message) => {
+      const { userId, nickname, isCorrect, drawingSrc, avatarsImgSrc } = message
+      setMyDrawing({
+        id: userId,
+        nickname,
+        isCorrect: isCorrect === 'CORRECT_ANSWER',
+        drawingImage: drawingSrc,
+        characterImage:
+          avatarsImgSrc === 'null' ? '/images/tiger.png' : avatarsImgSrc,
+      })
+    })
+  }, [registerCallback, sendMessage, user?.userId])
 
   useEffect(() => {
     const stageNumber = parseInt(localStorage.getItem('stageNumber') || '1') // 기본값 1 설정
@@ -19,12 +44,8 @@ export default function ScenarioResultParticipant() {
       router.push(`/scenario/1/situation/step${stageNumber + 1}`)
     }
 
-    registerCallback(
-      `/games/${localStorage.getItem('roomId')}`,
-      'GAME_START',
-      handleGameStart,
-    )
-  }, [registerCallback, router])
+    registerCallback(`/games/${roomId}`, 'GAME_START', handleGameStart)
+  }, [registerCallback, roomId, router])
 
   return (
     <div className="p-6 flex flex-col items-center">
@@ -33,13 +54,15 @@ export default function ScenarioResultParticipant() {
       </h2>
       <div className="relative">
         <ScoredBoard
-          data={{
-            id: '1',
-            isCorrect: false,
-            nickname: '이구역그림짱은나야 (나)',
-            characterImage: '/images/tiger.png',
-            drawingImage: '/images/drawing.png',
-          }}
+          data={
+            myDrawing || {
+              id: '1',
+              isCorrect: false,
+              nickname: '이구역그림짱은나야 (나)',
+              characterImage: '/images/tiger.png',
+              drawingImage: '/images/drawing.png',
+            }
+          }
         />
       </div>
       <AppealButton />
