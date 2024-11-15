667d15c2c3f43bfa1719631fcbbc1a70189e344c
yukyung <kyg0954@naver.com>
2024-11-15 15:41:30 +0900

    feat: 닉네임 변경 기능 추가

--- frontend/src/app/settings/components/NicknameInput/index.tsx
+++ frontend/src/app/settings/components/NicknameInput/index.tsx
@@ -1,13 +1,30 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
+import { User, useUser } from '@/app/_contexts/UserContext'
 
 export default function NicknameInput() {
-  const [nickname, setNickname] = useState<string>('햄벅유경')
+  const { user, setUser } = useUser()
+  const [nickname, setNickname] = useState<string>('')
 
   const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setNickname(e.target.value)
+
+  const handleClickEditButton = () => {
+    if (nickname.trim().length === 0) {
+      alert('닉네임을 입력해주세요')
+      return
+    }
+    setUser({ ...user, nickname } as User)
+    alert('닉네임이 변경되었습니다.')
+  }
+
+  useEffect(() => {
+    if (user?.nickname) {
+      setNickname(user?.nickname)
+    }
+  }, [user])
 
   return (
     <div className="relative grid grid-cols-[1fr_3fr] items-center">
@@ -18,13 +35,9 @@ export default function NicknameInput() {
         onChange={handleNicknameChange}
         className="w-full px-6 py-3 bg-gray-white border border-gray-dark hover:outline-primary-500 focus:outline-primary-500 rounded-xl text-3xl"
       />
-      <Image
-        src="/icons/pencil.svg"
-        alt="edit"
-        width={32}
-        height={32}
-        className="absolute right-4"
-      />
+      <button className="absolute right-4" onClick={handleClickEditButton}>
+        <Image src="/icons/pencil.svg" alt="edit" width={32} height={32} />
+      </button>
     </div>
   )
 }
